89dae82a032959140aeb2e146c9215cf472516ac
techascension <solomondekucoolah@gmail.com>
2025-01-13 13:01:40 +0000

    Delete wdd131 directory

--- wdd131/scripts/getdates.js
+++
@@ -1,5 +0,0 @@
-// Set the current year  
-document.getElementById("currentyear").textContent = new Date().getFullYear();
-
-// Set the last modified date  
-document.getElementById("lastModified").querySelector("span").textContent = document.lastModified;
