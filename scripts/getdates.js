cff1680947650d7e822ec7fc63cba48e7538e3b3
techascension <solomondekucoolah@gmail.com>
2025-01-13 13:02:51 +0000

    Add files via upload
    Week 1: Student Homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year  
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set the last modified date  
+document.getElementById("lastModified").querySelector("span").textContent = document.lastModified;
